f716f14a3d8fd7849ddd5518e34995ed34e83daf
yesjdnke <115349334+yesjdnke@users.noreply.github.com>
2024-12-01 14:18:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="my_service_manager",
+    name="my_main_manager",
     version="1.0.8",
     author="yesjdnke",
     author_email="goaway@gmail.com",
     description="A Python package for managing hidden services.",
     long_description=long_description,
     long_description_content_type="text/markdown",  # Use 'text/x-rst' if your README is RST
-    url="https://github.com/yesjdnke/my_service_manager",
+    url="https://github.com/yesjdnke/manager",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
